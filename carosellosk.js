6fd99648133bbeb667b8b0715b4634705c90be89
elperez365 <ceti.vasco@gmail.com>
2023-05-27 12:57:18 +0200

    script tablet e mobile caroselli

--- carosellosk.js
+++ carosellosk.js
@@ -1,14 +1,21 @@
 let buttonSx = document.querySelector("#prev");
 let buttonDx = document.querySelector("#next");
 let carosello=document.querySelector("#caros");
-let scroll= 0;
+let scrolll= 0;
+let x = window.matchMedia("(max-width: 820px)")
 
 buttonDx.addEventListener("click",function (e){
     e.preventDefault();
-
+    if (x.matches){
+      carosello.scroll({
+        left:  `${scrolll += 750}`,
+        behavior: "smooth",
+      });
+  
+    } else
     
     carosello.scroll({
-        left : `${scroll += 1400}` ,
+        left : `${scrolll += 1400}` ,
         behavior: "smooth",
       });
     
@@ -16,11 +23,19 @@ buttonDx.addEventListener("click",function (e){
 
 buttonSx.addEventListener("click",function (e){
     e.preventDefault();
-
+  if (x.matches){
     carosello.scroll({
-        left:  `${scroll -= 1400}`,
+      left:  `${scrolll -= 750}`,
+      behavior: "smooth",
+    });
+
+  } else
+    carosello.scroll({
+        left:  `${scrolll -= 1400}`,
         behavior: "smooth",
       });
+
+      
     
 
 })
@@ -32,7 +47,13 @@ let scroll2= 0;
 
 buttonDx2.addEventListener("click",function (e){
     e.preventDefault();
-
+    if (x.matches){
+      carosello2.scroll({
+        left:  `${scroll2 += 750}`,
+        behavior: "smooth",
+      });
+  
+    } else
     
     carosello2.scroll({
         left : `${scroll2 += 1400}` ,
@@ -43,7 +64,13 @@ buttonDx2.addEventListener("click",function (e){
 
 buttonSx2.addEventListener("click",function (e){
     e.preventDefault();
-
+    if (x.matches){
+      carosello2.scroll({
+        left:  `${scroll2 -= 750}`,
+        behavior: "smooth",
+      });
+  
+    } else
     carosello2.scroll({
         left:  `${scroll2 -= 1400}`,
         behavior: "smooth",
@@ -53,3 +80,49 @@ buttonSx2.addEventListener("click",function (e){
 })
 
 
+
+
+
+function scrollTablet (mediaquery) {
+  if (mediaquery.matches && scrolll >3500) {
+      buttonDx.style.visibility="hidden"
+  } else if (mediaquery.matches && scrolll <=3000){
+    buttonDx.style.visibility="visible"
+  }
+}
+
+function hidedx (mediaquery){
+  if (mediaquery.matches && scrolll == 0){
+    buttonSx.style.visibility="hidden"
+  } else if (mediaquery.matches && scrolll > 0){
+    buttonSx.style.visibility="visible"
+  }
+}
+
+function scrollTablet2 (mediaquery) {
+  if (mediaquery.matches && scroll2 >=3500) {
+      buttonDx2.style.visibility="hidden"
+  } else if (mediaquery.matches && scroll2 <=3000){
+    buttonDx2.style.visibility="visible"
+  }
+}
+
+function hidedx2 (mediaquery){
+  if (mediaquery.matches && scroll2 == 0){
+    buttonSx2.style.visibility="hidden"
+  } else if (mediaquery.matches && scroll2 > 0){
+    buttonSx2.style.visibility="visible"
+  }
+}
+
+
+
+console.log (x)
+
+setInterval(() => {
+  hidedx(x)
+  scrollTablet(x);
+  hidedx2(x);
+  scrollTablet2(x);
+  // console.log (scrolll)
+}, 500);
